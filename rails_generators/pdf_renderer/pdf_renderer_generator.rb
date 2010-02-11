539217eff1e3cab773e97e45440246b612d36719
Thomas Kadauke <tkadauke@imedo.de>
2010-02-11 05:09:27 +0100

    fix comments

--- rails_generators/pdf_renderer/pdf_renderer_generator.rb
+++ rails_generators/pdf_renderer/pdf_renderer_generator.rb
@@ -4,16 +4,16 @@ class PdfRendererGenerator < Rails::Generator::NamedBase
       # Check for class naming collisions.
       m.class_collisions "#{class_name}PdfRenderer", "#{class_name}PdfRendererTest"
 
-      # Mailer, view, test, and fixture directories.
+      # Renderer, view, and test directories.
       m.directory File.join('app/pdf_renderers', class_path)
       m.directory File.join('app/views', "#{file_path}_pdf_renderer")
       m.directory File.join('test/functional', class_path)
 
-      # Mailer class and unit test.
+      # Renderer class and functional test.
       m.template "pdf_renderer.rb",    File.join('app/pdf_renderers', class_path, "#{file_name}_pdf_renderer.rb")
       m.template "functional_test.rb", File.join('test/functional', class_path, "#{file_name}_pdf_renderer_test.rb")
 
-      # View template and fixture for each action.
+      # View template for each action.
       actions.each do |action|
         relative_path = File.join("#{file_path}_pdf_renderer", action)
         view_path     = File.join('app/views', "#{relative_path}.pdf.erb")
